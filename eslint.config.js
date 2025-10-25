9e0da187212c0599f1c0ced13a249c693a3eaac8
svaa21 <sotomayorvalerioadrian@gmail.com>
2025-10-25 10:19:59 -0500

    add eslint + prettier

---
+++ eslint.config.js
@@ -0,0 +1,15 @@
+import eslintPluginAstro from 'eslint-plugin-astro'
+export default [
+  // add more generic rule sets here, such as:
+  // js.configs.recommended,
+  ...eslintPluginAstro.configs.recommended,
+  {
+    rules: {
+      semi: ['error', 'never'],
+      indent: ['error', 2],
+      quotes: ['error', 'single'],
+      // override/add rules settings here, such as:
+      // "astro/no-set-html-directive": "error"
+    },
+  },
+]
